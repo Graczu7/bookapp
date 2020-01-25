3aa4919584e67bb5b2418d8c0f401f1e453820ae
Grzegorz Gracz <40638361+Graczu7@users.noreply.github.com>
2020-01-25 21:14:00 +0100

    Create CatalogConfiguration.java

---
+++ src/main/java/pl/gracz/bookapp/productatalog/CatalogConfiguration.java
@@ -0,0 +1,32 @@
+package pl.gracz.bookapp.productatalog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class CatalogConfiguration {
+
+    @Bean
+    ProductCatalogFacade facade(BookRepository repository) {
+        ProductCatalogFacade productCatalogFacade = new ProductCatalogFacade(repository);
+
+        productCatalogFacade.addBook(
+                thereIsPublishedBook("Hobbit"));
+        productCatalogFacade.addBook(
+                thereIsPublishedBook("Lord of the ring"));
+
+        return productCatalogFacade;
+    }
+
+    private Book thereIsPublishedBook(String title) {
+        return Book.builder()
+                .title(title)
+                .description(title)
+                .cover("https://picsum.photos/200/300")
+                .price(BigDecimal.valueOf(20.20))
+                .published(true)
+                .build();
+    }
+}
